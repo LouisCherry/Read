3b3412163ba5c33636680a5a8c7a925c410ef5c8
pan <2367257561@qq.com>
2016-05-22 09:13:28 +0800

    增加搜索控件

--- app/src/main/java/com/read/pan/MainActivity.java
+++ app/src/main/java/com/read/pan/MainActivity.java
@@ -3,6 +3,7 @@ package com.read.pan;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -19,7 +20,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.quinny898.library.persistentsearch.SearchBox;
+import com.quinny898.library.persistentsearch.SearchResult;
 import com.read.pan.activity.LoginActivity;
 import com.read.pan.adapter.FragmentAdapter;
 import com.read.pan.app.ReadApplication;
@@ -61,8 +65,13 @@ public class MainActivity extends AppCompatActivity
     List<String> tablayoutTitle;
     List<Fragment> viewPagerFragments;
     ReadApplication application;
+    @BindView(R.id.searchbox)
+    com.quinny898.library.persistentsearch.SearchBox searchbox;
+
     @Override
-    public void onFragmentInteraction(Uri uri){}
+    public void onFragmentInteraction(Uri uri) {
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,10 +91,10 @@ public class MainActivity extends AppCompatActivity
         tablayoutTitle.add(tablayoutTitle1);
         tablayoutTitle.add(tablayoutTitle2);
         //初始化ViewPager的数据集
-        viewPagerFragments=new ArrayList<>();
+        viewPagerFragments = new ArrayList<>();
         viewPagerFragments.add(new BookshelfFragment());
         viewPagerFragments.add(new StoreFragment());
-        application= (ReadApplication) getApplication();
+        application = (ReadApplication) getApplication();
     }
 
     /**
@@ -101,6 +110,14 @@ public class MainActivity extends AppCompatActivity
         //千万别忘了，关联TabLayout与ViewPager
         //同时也要覆写PagerAdapter的getPageTitle方法，否则Tab没有title
         tabLayout.setupWithViewPager(viewPager);
+        searchbox.enableVoiceRecognition(this);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                openSearch();
+                return true;
+            }
+        });
     }
 
     @Override
@@ -110,9 +127,9 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onResume() {
-        if(application.isLogin()){
-            User user=application.getLoginUser();
-            if(user!=null){
+        if (application.isLogin()) {
+            User user = application.getLoginUser();
+            if (user != null) {
                 navUsername.setText(user.getUserName());
             }
         }
@@ -140,13 +157,13 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        View navHeadView=navView.getHeaderView(0);
-        avatar= (ImageView) navHeadView.findViewById(R.id.nav_avatar);
-        navUsername= (TextView) navHeadView.findViewById(R.id.nav_username);
+        View navHeadView = navView.getHeaderView(0);
+        avatar = (ImageView) navHeadView.findViewById(R.id.nav_avatar);
+        navUsername = (TextView) navHeadView.findViewById(R.id.nav_username);
         avatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                if(!application.isLogin()){
+                if (!application.isLogin()) {
                     startActivity(new Intent(getBaseContext(), LoginActivity.class));
                 }
             }
@@ -189,11 +206,11 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        Intent intent=null;
+        Intent intent = null;
         switch (item.getItemId()) {
             case R.id.nav_imp:
                 //文件夹导入书籍
-                intent=new Intent(this, FileBrowserActivity.class);
+                intent = new Intent(this, FileBrowserActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
                 break;
@@ -215,5 +232,73 @@ public class MainActivity extends AppCompatActivity
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public void openSearch() {
+        toolbar.setTitle("");
+        searchbox.revealFromMenuItem(R.id.action_search, this);
+        for (int x = 0; x < 10; x++) {
+            SearchResult option = new SearchResult("Result "
+                    + Integer.toString(x), getResources().getDrawable(
+                    R.drawable.ic_history));
+            searchbox.addSearchable(option);
+        }
+        searchbox.setMenuListener(new SearchBox.MenuListener() {
+
+            @Override
+            public void onMenuClick() {
+                // Hamburger has been clicked
+                Toast.makeText(MainActivity.this, "Menu click",
+                        Toast.LENGTH_LONG).show();
+            }
+
+        });
+        searchbox.setSearchListener(new SearchBox.SearchListener() {
+
+            @Override
+            public void onSearchOpened() {
+                // Use this to tint the screen
+
+            }
+
+            @Override
+            public void onSearchClosed() {
+                // Use this to un-tint the screen
+                closeSearch();
+            }
+
+            @Override
+            public void onSearchTermChanged() {
+                // React to the search term changing
+                // Called after it has updated results
+            }
+
+            @Override
+            public void onSearch(String searchTerm) {
+                Toast.makeText(MainActivity.this, searchTerm + " Searched",
+                        Toast.LENGTH_LONG).show();
+                toolbar.setTitle(searchTerm);
+
+            }
+
+            @Override
+            public void onSearchCleared() {
+
+            }
+
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1234 && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data
+                    .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            searchbox.populateEditText(matches);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    protected void closeSearch() {
+        searchbox.hideCircularly(this);
+        if(searchbox.getSearchText().isEmpty())toolbar.setTitle("");
     }
 }
