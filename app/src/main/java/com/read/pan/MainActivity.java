c76fd10e18b34aec57cdc870bbebc6ca47d195b5
pan <2367257561@qq.com>
2016-05-27 09:14:48 +0800

    备份1

--- app/src/main/java/com/read/pan/MainActivity.java
+++ app/src/main/java/com/read/pan/MainActivity.java
@@ -19,14 +19,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
-import com.quinny898.library.persistentsearch.SearchBox;
-import com.quinny898.library.persistentsearch.SearchResult;
 import com.read.pan.activity.CollectActivity;
 import com.read.pan.activity.LoginActivity;
-import com.read.pan.activity.SearchResultActivity;
 import com.read.pan.activity.UserInfoActivity;
 import com.read.pan.adapter.FragmentAdapter;
 import com.read.pan.app.ReadApplication;
@@ -68,8 +64,8 @@ public class MainActivity extends AppCompatActivity
     List<String> tablayoutTitle;
     List<Fragment> viewPagerFragments;
     ReadApplication application;
-    @BindView(R.id.searchbox)
-    com.quinny898.library.persistentsearch.SearchBox searchbox;
+//    @BindView(R.id.searchbox)
+//    com.quinny898.library.persistentsearch.SearchBox searchbox;
     @Override
     public void onFragmentInteraction(Uri uri) {
     }
@@ -112,11 +108,11 @@ public class MainActivity extends AppCompatActivity
         //千万别忘了，关联TabLayout与ViewPager
         //同时也要覆写PagerAdapter的getPageTitle方法，否则Tab没有title
         tabLayout.setupWithViewPager(viewPager);
-        searchbox.enableVoiceRecognition(this);
+//        searchbox.enableVoiceRecognition(this);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                openSearch();
+//                openSearch();
                 return true;
             }
         });
@@ -243,70 +239,70 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
     public void openSearch() {
-        toolbar.setTitle("");
-        searchbox.revealFromMenuItem(R.id.action_search, this);
-        for (int x = 0; x < 10; x++) {
-            SearchResult option = new SearchResult("Result "
-                    + Integer.toString(x), getResources().getDrawable(
-                    R.drawable.ic_history));
-            searchbox.addSearchable(option);
-        }
-        searchbox.setMenuListener(new SearchBox.MenuListener() {
-
-            @Override
-            public void onMenuClick() {
-                // Hamburger has been clicked
-                Toast.makeText(MainActivity.this, "Menu click",
-                        Toast.LENGTH_LONG).show();
-            }
-
-        });
-        searchbox.setSearchListener(new SearchBox.SearchListener() {
-
-            @Override
-            public void onSearchOpened() {
-                // Use this to tint the screen
-
-            }
-
-            @Override
-            public void onSearchClosed() {
-                // Use this to un-tint the screen
-                closeSearch();
-            }
-
-            @Override
-            public void onSearchTermChanged() {
-                // React to the search term changing
-                // Called after it has updated results
-            }
-
-            @Override
-            public void onSearch(String searchTerm) {
-                startActivity(
-                        new Intent(getBaseContext(), SearchResultActivity.class)
-                                .putExtra("bookName",searchTerm));
-            }
-
-            @Override
-            public void onSearchCleared() {
-
-            }
-
-        });
+//        toolbar.setTitle("");
+//        searchbox.revealFromMenuItem(R.id.action_search, this);
+//        for (int x = 0; x < 10; x++) {
+//            SearchResult option = new SearchResult("Result "
+//                    + Integer.toString(x), getResources().getDrawable(
+//                    R.drawable.ic_history));
+//            searchbox.addSearchable(option);
+//        }
+//        searchbox.setMenuListener(new SearchBox.MenuListener() {
+//
+//            @Override
+//            public void onMenuClick() {
+//                // Hamburger has been clicked
+//                Toast.makeText(MainActivity.this, "Menu click",
+//                        Toast.LENGTH_LONG).show();
+//            }
+//
+//        });
+//        searchbox.setSearchListener(new SearchBox.SearchListener() {
+//
+//            @Override
+//            public void onSearchOpened() {
+//                // Use this to tint the screen
+//
+//            }
+//
+//            @Override
+//            public void onSearchClosed() {
+//                // Use this to un-tint the screen
+//                closeSearch();
+//            }
+//
+//            @Override
+//            public void onSearchTermChanged() {
+//                // React to the search term changing
+//                // Called after it has updated results
+//            }
+//
+//            @Override
+//            public void onSearch(String searchTerm) {
+//                startActivity(
+//                        new Intent(getBaseContext(), SearchResultActivity.class)
+//                                .putExtra("bookName",searchTerm));
+//            }
+//
+//            @Override
+//            public void onSearchCleared() {
+//
+//            }
+//
+//        });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1234 && resultCode == RESULT_OK) {
             ArrayList<String> matches = data
                     .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            searchbox.populateEditText(matches);
+//            searchbox.populateEditText(matches);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
     protected void closeSearch() {
-        searchbox.hideCircularly(this);
-        if(searchbox.getSearchText().isEmpty())toolbar.setTitle("");
+//        searchbox.hideCircularly(this);
+//        if(searchbox.getSearchText().isEmpty())toolbar.setTitle("");
     }
 }
